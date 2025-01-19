7c212c13778d22445378c25e2ae8ba6da29c1981
dwl21 <nggus5@gmail.com>
2025-01-19 11:56:40 +0900

    chore: DB Cleaner 설정

---
+++ src/test/kotlin/com/yourssu/soongpt/common/support/ApplicationTest.kt
@@ -0,0 +1,11 @@
+package com.yourssu.soongpt.common.support
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@SpringBootTest
+@SuppressWarnings("NonAsciiCharacters")
+@ExtendWith(DataClearExtension::class)
+annotation class ApplicationTest
