30e75a0112fe9d9a87074bf10805927f8a54a967
dwl21 <nggus5@gmail.com>
2025-01-19 12:05:41 +0900

    style: rename a package of DB cleaner

--- src/test/kotlin/com/yourssu/soongpt/common/support/config/ApplicationTest.kt
+++ src/test/kotlin/com/yourssu/soongpt/common/support/config/ApplicationTest.kt
@@ -1,4 +1,4 @@
-package com.yourssu.soongpt.common.support
+package com.yourssu.soongpt.common.support.config
 
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
