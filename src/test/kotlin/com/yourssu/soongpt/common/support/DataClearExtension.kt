d56a97a1a20d9119e675c3be7a2ef8b2d77c0c55
dwl21 <nggus5@gmail.com>
2025-01-19 11:56:40 +0900

    chore: DB Cleaner 설정

---
+++ src/test/kotlin/com/yourssu/soongpt/common/support/DataClearExtension.kt
@@ -0,0 +1,13 @@
+package com.yourssu.soongpt.common.support
+
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+class DataClearExtension : BeforeEachCallback {
+    override fun beforeEach(context: ExtensionContext?) {
+        SpringExtension.getApplicationContext(context!!)
+            .getBean(DatabaseCleaner::class.java)
+            .execute()
+    }
+}
