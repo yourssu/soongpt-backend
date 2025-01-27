89ddc5f048bdf5df4b8b390ee7eef06e0808def7
dwl21 <nggus5@gmail.com>
2025-01-27 22:18:40 +0900

    feat: 과목 아이디로 수강 시간 조회 기능 구현

--- src/test/kotlin/com/yourssu/soongpt/domain/courseTime/storage/CourseTimeRepositoryImplTest.kt
+++ src/test/kotlin/com/yourssu/soongpt/domain/courseTime/storage/CourseTimeRepositoryImplTest.kt
@@ -1,8 +1,10 @@
 package com.yourssu.soongpt.domain.courseTime.storage
 
 import com.yourssu.soongpt.common.support.config.ApplicationTest
+import com.yourssu.soongpt.common.support.fixture.CourseTimeFixture.MONDAY_17_19
+import com.yourssu.soongpt.common.support.fixture.CourseTimeFixture.TUESDAY_17_19
 import com.yourssu.soongpt.domain.courseTime.implement.CourseTimeRepository
-import com.yourssu.soongpt.domain.courseTime.storage.exception.CourseTimeNotFoundException
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import kotlin.test.Test
@@ -14,14 +16,22 @@ class CourseTimeRepositoryImplTest {
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
-    inner class get_메서드는 {
+    inner class findAllByCourseId_메서드는 {
+        private val courseId = 1L
+        @BeforeEach
+        fun setUp() {
+            courseTimeRepository.save(TUESDAY_17_19.toCourseTime(courseId))
+            courseTimeRepository.save(MONDAY_17_19.toCourseTime(courseId))
+        }
+
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
-        inner class 아이디에_해당하는_강의_시간이_없으면 {
+        inner class 과목_아이디를_받으면 {
             @Test
-            @DisplayName("CourseTimeNotFound 예외를 던진다.")
-            fun failure() {
-                assertThrows<CourseTimeNotFoundException> { courseTimeRepository.get(0L) }
+            @DisplayName("수강 시간 목록을 반환한다.")
+            fun success() {
+                val courseTimes = courseTimeRepository.findAllByCourseId(courseId)
+                assertThat(courseTimes).hasSize(2)
             }
         }
     }
