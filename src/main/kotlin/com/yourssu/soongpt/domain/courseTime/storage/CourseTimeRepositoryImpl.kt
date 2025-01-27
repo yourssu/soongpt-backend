b115a78f20b3077178897980963154355a6ff2ba
dwl21 <nggus5@gmail.com>
2025-01-27 22:18:40 +0900

    feat: 과목 아이디로 수강 시간 조회 기능 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/courseTime/storage/CourseTimeRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/courseTime/storage/CourseTimeRepositoryImpl.kt
@@ -1,26 +1,29 @@
 package com.yourssu.soongpt.domain.courseTime.storage
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import com.yourssu.soongpt.domain.courseTime.implement.CourseTime
 import com.yourssu.soongpt.domain.courseTime.implement.CourseTimeRepository
-import com.yourssu.soongpt.domain.courseTime.storage.exception.CourseTimeNotFoundException
+import com.yourssu.soongpt.domain.courseTime.storage.QCourseTimeEntity.courseTimeEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
 class CourseTimeRepositoryImpl(
-        private val courseTimeJpaRepository: CourseTimeJpaRepository
+    private val courseTimeJpaRepository: CourseTimeJpaRepository,
+    private val jpaQueryFactory: JPAQueryFactory,
 ) : CourseTimeRepository {
     override fun save(courseTime: CourseTime): CourseTime {
         return courseTimeJpaRepository.save(CourseTimeEntity.from(courseTime))
             .toDomain()
     }
 
-    override fun get(id: Long): CourseTime {
-        val courseTime =  courseTimeJpaRepository.findByIdOrNull(id)
-            ?: throw CourseTimeNotFoundException()
-        return courseTime.toDomain()
+    override fun findAllByCourseId(courseId: Long): List<CourseTime> {
+        return jpaQueryFactory.selectFrom(courseTimeEntity)
+            .where(courseTimeEntity.courseId.eq(courseId))
+            .fetch()
+            .map { it.toDomain() }
     }
 }
 
-interface CourseTimeJpaRepository: JpaRepository<CourseTimeEntity, Long>
+interface CourseTimeJpaRepository : JpaRepository<CourseTimeEntity, Long> {
+}
