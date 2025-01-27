6705c48d2393aefb870b8dacdf8d5d3a0ca4b01f
dwl21 <nggus5@gmail.com>
2025-01-27 22:18:40 +0900

    feat: 과목 아이디로 수강 시간 조회 기능 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/courseTime/implement/CourseTimeRepository.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/courseTime/implement/CourseTimeRepository.kt
@@ -2,5 +2,5 @@ package com.yourssu.soongpt.domain.courseTime.implement
 
 interface CourseTimeRepository {
     fun save(courseTime: CourseTime): CourseTime
-    fun get(id: Long): CourseTime
+    fun findAllByCourseId(courseId: Long): List<CourseTime>
 }
