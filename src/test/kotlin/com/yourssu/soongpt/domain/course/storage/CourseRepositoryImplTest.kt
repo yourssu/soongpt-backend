55cf7b508799c9e9d7c9bdc62bd6ea8690ab2a35
dwl21 <nggus5@gmail.com>
2025-01-28 15:27:38 +0900

    feat: 학과 아이디로 과목 조회 기능 구현

--- src/test/kotlin/com/yourssu/soongpt/domain/course/storage/CourseRepositoryImplTest.kt
+++ src/test/kotlin/com/yourssu/soongpt/domain/course/storage/CourseRepositoryImplTest.kt
@@ -1,8 +1,15 @@
 package com.yourssu.soongpt.domain.course.storage
 
 import com.yourssu.soongpt.common.support.config.ApplicationTest
+import com.yourssu.soongpt.common.support.fixture.CourseFixture.MAJOR_CORE
+import com.yourssu.soongpt.common.support.fixture.DepartmentFixture.COMPUTER
+import com.yourssu.soongpt.common.support.fixture.DepartmentGradeFixture.FIRST
+import com.yourssu.soongpt.common.support.fixture.TargetFixture
 import com.yourssu.soongpt.domain.course.implement.CourseRepository
-import com.yourssu.soongpt.domain.course.storage.exception.CourseNotFoundException
+import com.yourssu.soongpt.domain.department.implement.DepartmentRepository
+import com.yourssu.soongpt.domain.departmentGrade.implement.DepartmentGradeRepository
+import com.yourssu.soongpt.domain.target.implement.TargetRepository
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import kotlin.test.Test
@@ -12,16 +19,46 @@ class CourseRepositoryImplTest {
     @Autowired
     private lateinit var courseRepository: CourseRepository
 
+    @Autowired
+    private lateinit var departmentRepository: DepartmentRepository
+
+    @Autowired
+    private lateinit var departmentGradeRepository: DepartmentGradeRepository
+
+    @Autowired
+    private lateinit var targetRepository: TargetRepository
+
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
-    inner class get_메서드는 {
+    inner class findAllByDepartmentId_메서드는 {
+        var departmentId: Long? = null
+
+        @BeforeEach
+        fun setUp() {
+            val course = courseRepository.save(MAJOR_CORE.toDomainRandomCourseCode())
+            val department = departmentRepository.save(COMPUTER.toDomain(1L))
+            departmentId = department.id
+            val departmentGrade = departmentGradeRepository.save(FIRST.toDomain(departmentId = departmentId!!))
+            targetRepository.save(
+                TargetFixture.TARGET1.toDomain(
+                    departmentGradeId = departmentGrade.id!!,
+                    courseId = course.id!!
+                )
+            )
+        }
+
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
-        inner class 아이디에_해당하는_과목이_없으면 {
+        inner class 학과_아이디와_이수구분을_받으면 {
             @Test
-            @DisplayName("CourseNotFound 예외를 던진다.")
-            fun failure() {
-                assertThrows<CourseNotFoundException> { courseRepository.get(0L) }
+            @DisplayName("해당하는 과목을 반환한다.")
+            fun success() {
+                val courses = courseRepository.findAllByDepartmentId(
+                    departmentId = departmentId!!,
+                    classification = MAJOR_CORE.classification
+                )
+
+                assertThat(courses).hasSize(1)
             }
         }
     }
