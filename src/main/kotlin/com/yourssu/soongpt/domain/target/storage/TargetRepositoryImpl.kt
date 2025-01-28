e7c51cd2dfe8d4f952f465a3266ff38ef88e4604
dwl21 <nggus5@gmail.com>
2025-01-28 14:36:59 +0900

    feat: 과목 아이디로 수강 대상 조회 기능 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/target/storage/TargetRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/target/storage/TargetRepositoryImpl.kt
@@ -1,25 +1,27 @@
 package com.yourssu.soongpt.domain.target.storage
 
+import com.querydsl.jpa.impl.JPAQueryFactory
 import com.yourssu.soongpt.domain.target.implement.Target
 import com.yourssu.soongpt.domain.target.implement.TargetRepository
-import com.yourssu.soongpt.domain.target.storage.exception.TargetNotFoundException
+import com.yourssu.soongpt.domain.target.storage.QTargetEntity.targetEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
 class TargetRepositoryImpl(
     private val targetJpaRepository: TargetJpaRepository,
+    private val jpaQueryFactory: JPAQueryFactory,
 ): TargetRepository {
     override fun save(target: Target): Target {
         return targetJpaRepository.save(TargetEntity.from(target))
             .toDomain()
     }
 
-    override fun get(id: Long): Target {
-        val target = targetJpaRepository.findByIdOrNull(id)
-            ?: throw TargetNotFoundException()
-        return target.toDomain()
+    override fun findAllByCourseId(courseId: Long): List<Target> {
+        return jpaQueryFactory.selectFrom(targetEntity)
+            .where(targetEntity.courseId.eq(courseId))
+            .fetch()
+            .map { it.toDomain() }
     }
 }
 
