435705a860ea1b9e072e8e684f9e1b7145ce32e3
dwl21 <nggus5@gmail.com>
2025-01-28 15:27:38 +0900

    feat: 학과 아이디로 과목 조회 기능 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseRepository.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseRepository.kt
@@ -2,5 +2,5 @@ package com.yourssu.soongpt.domain.course.implement
 
 interface CourseRepository {
     fun save(course: Course): Course
-    fun get(id: Long): Course
+    fun findAllByDepartmentId(departmentId: Long, classification: Classification): List<Course>
 }
--- src/main/kotlin/com/yourssu/soongpt/domain/course/storage/CourseRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/storage/CourseRepositoryImpl.kt
@@ -1,25 +1,37 @@
 package com.yourssu.soongpt.domain.course.storage
 
+import com.querydsl.jpa.impl.JPAQueryFactory
+import com.yourssu.soongpt.domain.course.implement.Classification
 import com.yourssu.soongpt.domain.course.implement.Course
 import com.yourssu.soongpt.domain.course.implement.CourseRepository
-import com.yourssu.soongpt.domain.course.storage.exception.CourseNotFoundException
+import com.yourssu.soongpt.domain.course.storage.QCourseEntity.courseEntity
+import com.yourssu.soongpt.domain.departmentGrade.storage.QDepartmentGradeEntity.departmentGradeEntity
+import com.yourssu.soongpt.domain.target.storage.QTargetEntity.targetEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
 class CourseRepositoryImpl(
     private val courseJpaRepository: CourseJpaRepository,
+    private val jpaQueryFactory: JPAQueryFactory,
 ) : CourseRepository {
     override fun save(course: Course): Course {
         return courseJpaRepository.save(CourseEntity.from(course))
             .toDomain()
     }
 
-    override fun get(id: Long): Course {
-        val course = courseJpaRepository.findByIdOrNull(id)
-            ?: throw CourseNotFoundException()
-        return course.toDomain()
+    override fun findAllByDepartmentId(departmentId: Long, classification: Classification): List<Course> {
+        return jpaQueryFactory.selectFrom(courseEntity)
+            .innerJoin(targetEntity)
+            .on(courseEntity.id.eq(targetEntity.courseId))
+            .innerJoin(departmentGradeEntity)
+            .on(targetEntity.departmentGradeId.eq(departmentGradeEntity.id))
+            .where(
+                departmentGradeEntity.departmentId.eq(departmentId),
+                courseEntity.classification.eq(classification)
+            )
+            .fetch()
+            .map { it.toDomain() }
     }
 }
 
