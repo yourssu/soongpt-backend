6bbbb322065e0d626968355631c2faeaa8359f33
dwl21 <nggus5@gmail.com>
2025-01-30 12:15:26 +0900

    style: 생성자 테스트 문구 변경

--- src/test/kotlin/com/yourssu/soongpt/domain/courseTime/implement/TimeTest.kt
+++ src/test/kotlin/com/yourssu/soongpt/domain/courseTime/implement/TimeTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.*
 class TimeTest {
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
-    inner class 생성자_메서드는 {
+    inner class 생성자는 {
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores::class)
         inner class 하루를_분으로_환산한_값의_범위가_아니면 {
