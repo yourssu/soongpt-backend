ed44d7d3b876b033ca3285a77ebd0eb0576bf2d2
dwl21 <nggus5@gmail.com>
2025-01-31 20:07:23 +0900

    feat: 해당 학과의 단일과목 수강 대상 조회 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/target/implement/TargetReader.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/target/implement/TargetReader.kt
@@ -1,12 +1,27 @@
 package com.yourssu.soongpt.domain.target.implement
 
+import com.yourssu.soongpt.domain.department.implement.Department
+import com.yourssu.soongpt.domain.departmentGrade.implement.DepartmentGrade
+import com.yourssu.soongpt.domain.departmentGrade.implement.DepartmentGradeRepository
 import org.springframework.stereotype.Component
 
 @Component
 class TargetReader(
     private val targetRepository: TargetRepository,
+    private val departmentGradeRepository: DepartmentGradeRepository,
 ) {
-    fun findAllByCourseId(courseId: Long): List<Target> {
+    fun findAllBy(courseId: Long, department: Department): List<String> {
+        val targets = findAllByCourseId(courseId)
+            .map { departmentGradeRepository.get(it.departmentGradeId) }
+            .filter { it.departmentId == department.id }
+        return targets.map { formatTargetDisplayName(department, it) }
+    }
+
+    private fun findAllByCourseId(courseId: Long): List<Target> {
         return targetRepository.findAllByCourseId(courseId)
+    }
+
+    private fun formatTargetDisplayName(department: Department, departmentGrade: DepartmentGrade): String {
+        return "${department.name}${departmentGrade.grade}"
     }
 }
