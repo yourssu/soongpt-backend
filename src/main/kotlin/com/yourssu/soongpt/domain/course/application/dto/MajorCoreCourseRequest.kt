07f204cc6681f34674886215cd697dc8d728c61c
dwl21 <nggus5@gmail.com>
2025-01-31 20:08:11 +0900

    feat: 전공 필수 과목 조회 API 구현

---
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/application/dto/MajorCoreCourseRequest.kt
@@ -0,0 +1,16 @@
+package com.yourssu.soongpt.domain.course.application.dto
+
+import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.Range
+
+data class MajorCoreCourseRequest(
+    @Range(min = 15, max = 25, message = "학번은 15부터 25까지 가능합니다.")
+    val schoolId: Long,
+
+    @NotBlank
+    val department: String,
+
+    @Range(min = 1, max = 5, message = "학년은 1부터 5까지 가능합니다.")
+    val grade: Int,
+) {
+}
