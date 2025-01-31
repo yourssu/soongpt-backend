c411bc7b59e66f11477ffe43ef5b8110c4ab9fd6
dwl21 <nggus5@gmail.com>
2025-01-31 20:00:50 +0900

    feat: 요일, 이수구분 displayName 설정

--- src/main/kotlin/com/yourssu/soongpt/domain/courseTime/implement/Week.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/courseTime/implement/Week.kt
@@ -1,11 +1,13 @@
 package com.yourssu.soongpt.domain.courseTime.implement
 
-enum class Week {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY,
+enum class Week(
+    val displayName: String,
+) {
+    MONDAY("mon"),
+    TUESDAY("tue"),
+    WEDNESDAY("wed"),
+    THURSDAY("thu"),
+    FRIDAY("fri"),
+    SATURDAY("sat"),
+    SUNDAY("sun");
 }
