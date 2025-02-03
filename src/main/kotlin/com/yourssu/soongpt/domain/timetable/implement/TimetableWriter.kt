d214e8f2d32dd60edfce8645439a8611c4ae7767
dwl21 <nggus5@gmail.com>
2025-02-03 17:26:12 +0900

    feat: 시간표 Reader, Writer 구현

---
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimetableWriter.kt
@@ -0,0 +1,12 @@
+package com.yourssu.soongpt.domain.timetable.implement
+
+import org.springframework.stereotype.Component
+
+@Component
+class TimetableWriter(
+    private val timetableRepository: TimetableRepository,
+) {
+    fun save(timetable: Timetable): Timetable {
+        return timetableRepository.save(timetable)
+    }
+}
