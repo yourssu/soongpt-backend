dbc860ec6e460374a2643e4ce5652792746b626d
dwl21 <nggus5@gmail.com>
2025-02-03 17:25:39 +0900

    feat: 시간표 도메인 구현

---
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/storage/TimetableEntity.kt
@@ -0,0 +1,33 @@
+package com.yourssu.soongpt.domain.timetable.storage
+
+import com.yourssu.soongpt.domain.timetable.implement.Tag
+import com.yourssu.soongpt.domain.timetable.implement.Timetable
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "timetable")
+class TimetableEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    val tag: Tag,
+) {
+    companion object {
+        fun from(timetable: Timetable): TimetableEntity {
+            return TimetableEntity(
+                id = timetable.id,
+                tag = timetable.tag
+            )
+        }
+    }
+
+    fun toDomain(): Timetable {
+        return Timetable(
+            id = id,
+            tag = tag
+        )
+    }
+}
