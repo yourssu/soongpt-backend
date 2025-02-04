71812e1c49dc0c7485cdb25d356f322ce4380aa4
dwl21 <nggus5@gmail.com>
2025-02-04 22:56:18 +0900

    feat: 평점 저장, 조회 API 구현

---
+++ src/main/kotlin/com/yourssu/soongpt/domain/rating/implement/RatingWriter.kt
@@ -0,0 +1,12 @@
+package com.yourssu.soongpt.domain.rating.implement
+
+import org.springframework.stereotype.Component
+
+@Component
+class RatingWriter(
+    private val ratingRepository: RatingRepository,
+) {
+    fun save(rating: Rating): Rating {
+        return ratingRepository.save(rating)
+    }
+}
