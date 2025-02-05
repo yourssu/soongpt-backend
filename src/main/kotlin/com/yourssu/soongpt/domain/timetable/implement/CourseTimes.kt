15ab5ae9e087e21ef4076aad1fd8f16caeece102
dwl21 <nggus5@gmail.com>
2025-02-06 01:29:32 +0900

    feat: 과목 시간 일급 컬렉션

---
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/CourseTimes.kt
@@ -0,0 +1,31 @@
+package com.yourssu.soongpt.domain.timetable.implement
+
+import com.yourssu.soongpt.domain.courseTime.implement.CourseTime
+import com.yourssu.soongpt.domain.courseTime.implement.Week
+
+class CourseTimes(
+    private val courseTimes: List<CourseTime>
+) {
+    fun hasOverlappingCourseTimes(): Boolean {
+        for (i in courseTimes.indices) {
+            for (j in i + 1 until courseTimes.size) {
+                if (courseTimes[i].week == courseTimes[j].week &&
+                    courseTimes[j].endTime.isOverThan(courseTimes[i].startTime) &&
+                    courseTimes[i].endTime.isOverThan(courseTimes[j].startTime)
+                ) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    fun hasFreeDay(): Boolean {
+        for (day in Week.entries) {
+            if (courseTimes.none { it.week == day }) {
+                return true
+            }
+        }
+        return false
+    }
+}
