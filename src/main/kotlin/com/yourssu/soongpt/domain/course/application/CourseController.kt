9a5256559a8b79276962cd305a5adb97c5833504
chaeyeon0130 <k84292330@gmail.com>
2025-02-05 18:47:13 +0900

    feat: 과목 생성 api 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/course/application/CourseController.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/application/CourseController.kt
@@ -1,6 +1,7 @@
 package com.yourssu.soongpt.domain.course.application
 
 import com.yourssu.soongpt.common.business.dto.Response
+import com.yourssu.soongpt.domain.course.application.dto.CreateCourseRequest
 import com.yourssu.soongpt.domain.course.application.dto.MajorRequiredCourseRequest
 import com.yourssu.soongpt.domain.course.business.CourseService
 import com.yourssu.soongpt.domain.course.business.dto.CourseResponse
@@ -8,6 +9,8 @@ import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -20,5 +23,11 @@ class CourseController(
     fun getMajorRequiredCourses(@Valid @ModelAttribute request: MajorRequiredCourseRequest): ResponseEntity<Response<List<CourseResponse>>> {
         val response = courseService.findByDepartmentNameInMajorRequired(request.department)
         return ResponseEntity.ok().body(Response(result = response))
+    }
+
+    @PostMapping
+    fun createCourses(@RequestBody courses: List<CreateCourseRequest>): ResponseEntity<String> {
+        courseService.createCourses(courses)
+        return ResponseEntity.ok("과목 정보 저장 완료")
     }
 }
--- src/main/kotlin/com/yourssu/soongpt/domain/department/implement/DepartmentReader.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/department/implement/DepartmentReader.kt
@@ -11,4 +11,19 @@ class DepartmentReader(
         return departmentRepository.findByName(name)
             ?: throw DepartmentNotFoundByNameException()
     }
+
+    fun getMatchingDepartments(name: String): List<Department> {
+        if (name.equals("경제학과")) {
+            val dept = departmentRepository.findByName(name)
+            return if (dept != null) listOf(dept) else emptyList()
+        }
+        val matchedDepartments = departmentRepository.findAll().filter { dept ->
+            dept.name.contains(name) || name.contains(dept.name)
+        }
+        return matchedDepartments
+    }
+
+    fun getAll(): List<Department> {
+        return departmentRepository.findAll()
+    }
 }
