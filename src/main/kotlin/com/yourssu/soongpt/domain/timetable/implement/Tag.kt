3cff0c7b677584b3150ff397268a61c390f055e4
dwl21 <nggus5@gmail.com>
2025-02-06 01:28:57 +0900

    feat: 태그 추가

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/Tag.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/Tag.kt
@@ -1,6 +1,12 @@
 package com.yourssu.soongpt.domain.timetable.implement
 
-enum class Tag {
-    DEFAULT,
+enum class Tag(val description: String) {
+    HAS_FREE_DAY("공강 날이 있는 시간표"),
+    NO_MORNING_CLASSES("아침 수업이 없는 시간표"),
+    NO_LONG_BREAKS("우주 공강이 없는 시간표"),
+    EVENLY_DISTRIBUTED("균등하게 배분되어 있는 시간표"),
+    GUARANTEED_LUNCH_TIME("점심시간 보장되는 시간표"),
+    NO_EVENING_CLASSES("저녁수업이 없는 시간표"),
+    DEFAULT("기본 태그")
     ;
 }
