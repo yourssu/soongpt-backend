a93b25f37ae407dc948168902c3345b460e6a5cb
dwl21 <nggus5@gmail.com>
2025-02-06 19:23:51 +0900

    feat: 시간표 후보들에 과목을 추가하는 코드 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimetableCandidates.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimetableCandidates.kt
@@ -1,9 +1,23 @@
 package com.yourssu.soongpt.domain.timetable.implement
 
+import com.yourssu.soongpt.domain.course.implement.Courses
+
 class TimetableCandidates(
     val values: List<TimetableCandidate>
 ) {
-    fun filterTagStrategy(): TimetableCandidates {
-        return TimetableCandidates(values.filter { it.isCorrect() })
+    fun filterRules(): TimetableCandidates {
+        return TimetableCandidates(values.filter { timetableRules(it) })
+    }
+
+    fun extendCourses(addCourses: List<Pair<Courses, CourseTimes>>): TimetableCandidates {
+        return TimetableCandidates(addCourses.flatMap { (courses, courseTimes) ->
+            values.map {
+                it.generateNewTimetableCandidate(courses, courseTimes)
+            }
+        }.filter { timetableRules(it) })
+    }
+
+    private fun timetableRules(timetableCandidate: TimetableCandidate): Boolean {
+        return timetableCandidate.isCorrect() && timetableCandidate.isCorrectCreditRule() && timetableCandidate.hasOverlappingCourseTimes()
     }
 }
