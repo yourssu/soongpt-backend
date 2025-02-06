0e728b51805897fb9c9c3af61fb7632603462733
dwl21 <nggus5@gmail.com>
2025-02-07 02:18:22 +0900

    feat: 과목 조합이 학점을 넘지 않도록 설계

--- src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CoursesFactory.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CoursesFactory.kt
@@ -44,9 +44,35 @@ class CoursesFactory(
         return CoursesFactory(combinations.map { Courses(it) })
     }
 
+    fun allCasesLessThan(standard: Int): List<Courses> {
+        val combinations = mutableListOf<List<Course>>(emptyList())
+        for (courses in values) {
+            val newCombinations = mutableListOf<List<Course>>()
+            for (currentCombination in combinations) {
+                for (course in courses.values) {
+                    if (currentCombination.sumOf { it.credit } + course.credit > standard) {
+                        continue
+                    }
+                    newCombinations.add(currentCombination + course)
+                }
+            }
+            combinations.addAll(newCombinations)
+        }
 
+        return combinations.map { Courses(it) }
+    }
 
     fun filterLessThanTotalCredit(standard: Int): List<Courses> {
         return values.filter { it.totalCredit() < standard }
+    }
+
+    fun sortByRatingAverage(ratings: Map<Long, Double>): List<Courses> {
+        return values.sortedWith(
+            compareBy(
+                { courses -> -courses.totalCredit() },
+                { courses -> -(courses.values.sumOf { course -> ratings[course.id]!! } / courses.values.size) },
+                { courses -> -courses.values.size }
+            )
+        )
     }
 }
