e387375c6217face57aa30c89d3768176820c070
dwl21 <nggus5@gmail.com>
2025-02-06 19:22:34 +0900

    feat: 과목 일급 컬렉션 기능 추가

--- src/main/kotlin/com/yourssu/soongpt/domain/course/implement/Courses.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/implement/Courses.kt
@@ -7,11 +7,35 @@ class Courses(
         return values.isEmpty()
     }
 
+    fun totalCredit(): Int {
+        return values.sumOf { it.credit }
+    }
+
+    fun getFirstCredit(): Int {
+        if (values.isEmpty()) {
+            return 0
+        }
+        return values.first().credit
+    }
+
     fun unpackNameAndProfessor(): List<Pair<String, String>> {
         return values.map { it.courseName to it.professorName!! }
     }
 
     fun getAllIds(): List<Long> {
         return values.map { it.id!! }
+    }
+
+    fun groupByCourseNames(): List<Courses> {
+        return values.groupBy { it.courseName }
+            .map { Courses(it.value) }
+    }
+
+    fun add(course: Course): Courses {
+        return Courses(this.values + course)
+    }
+
+    fun extend(courses: Courses): Courses {
+        return Courses(this.values + courses.values)
     }
 }
