2340dfe352ac5a684fa83d0a193f03f27d71be6e
dwl21 <nggus5@gmail.com>
2025-02-06 23:26:06 +0900

    refactor: 시간표 발행을 하는 factory 주체 변경

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimeTableFactory.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimeTableFactory.kt
@@ -4,9 +4,12 @@ import com.yourssu.soongpt.domain.course.implement.CourseReader
 import com.yourssu.soongpt.domain.course.implement.Courses
 import com.yourssu.soongpt.domain.course.implement.Courses.Companion.calculateAvailableMajorElective
 import com.yourssu.soongpt.domain.course.implement.CoursesFactory
+import com.yourssu.soongpt.domain.courseTime.implement.CourseTimeReader
 import com.yourssu.soongpt.domain.department.implement.DepartmentReader
 import com.yourssu.soongpt.domain.departmentGrade.implement.DepartmentGradeReader
+import com.yourssu.soongpt.domain.timetable.business.dto.TimetableCourseResponse
 import com.yourssu.soongpt.domain.timetable.business.dto.TimetableCreatedCommand
+import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponse
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
@@ -15,9 +18,11 @@ class TimeTableFactory(
     private val departmentReader: DepartmentReader,
     private val departmentGradeReader: DepartmentGradeReader,
     private val courseReader: CourseReader,
-    private val timetableCandidateFactory: TimetableCandidateFactory
+    private val courseTimeReader: CourseTimeReader,
+    private val timetableWriter: TimetableWriter,
+    private val timetableCourseWriter: TimetableCourseWriter,
+    private val timetableCandidateFactory: TimetableCandidateFactory,
 ) {
-    @Transactional
     fun createTimetable(command: TimetableCreatedCommand): TimetableCandidates {
         val department = departmentReader.getByName(command.departmentName)
         val departmentGrade = departmentGradeReader.getByDepartmentIdAndGrade(department.id!!, command.grade)
@@ -45,4 +50,30 @@ class TimeTableFactory(
 
         return timetableCandidateFactory.extendTimetableCandidates(timetableCandidates, addMajorElectives)
     }
+
+    @Transactional
+    fun issueTimetables(timetableCandidates: TimetableCandidates): List<TimetableResponse> {
+        val responses = ArrayList<TimetableResponse>()
+        for (timetableCandidate in timetableCandidates.values) {
+            val timetable = timetableWriter.save(Timetable(tag = timetableCandidate.tag))
+            saveTimetableCourses(timetableCandidate.courses, timetable)
+            responses.add(
+                TimetableResponse(
+                    timetable.id!!,
+                    timetable.tag.name,
+                    toTimetableCourseResponses(timetableCandidate.courses)
+                )
+            )
+        }
+        return responses
+    }
+
+    private fun saveTimetableCourses(courses: Courses, timetable: Timetable) {
+        for (course in courses.values) {
+            timetableCourseWriter.save(TimetableCourse(timetableId = timetable.id!!, courseId = course.id!!))
+        }
+    }
+
+    private fun toTimetableCourseResponses(courses: Courses) =
+        courses.values.map { TimetableCourseResponse.from(it, courseTimeReader.findAllByCourseId(it.id!!)) }
 }
--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimetableCandidateFactory.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimetableCandidateFactory.kt
@@ -2,16 +2,11 @@ package com.yourssu.soongpt.domain.timetable.implement
 
 import com.yourssu.soongpt.domain.course.implement.Courses
 import com.yourssu.soongpt.domain.courseTime.implement.CourseTimeReader
-import com.yourssu.soongpt.domain.timetable.business.dto.TimetableCourseResponse
-import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponse
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 
 @Component
 class TimetableCandidateFactory(
     private val courseTimeReader: CourseTimeReader,
-    private val timetableWriter: TimetableWriter,
-    private val timetableCourseWriter: TimetableCourseWriter,
 ) {
     fun createTimetableCandidates(coursesCandidates: List<Courses>): TimetableCandidates {
         return TimetableCandidates(coursesCandidates.flatMap {
@@ -28,30 +23,4 @@ class TimetableCandidateFactory(
             )
         })
     }
-
-    @Transactional
-    fun issueTimetables(timetableCandidates: TimetableCandidates): ArrayList<TimetableResponse> {
-        val responses = ArrayList<TimetableResponse>()
-        for (timetableCandidate in timetableCandidates.values) {
-            val timetable = timetableWriter.save(Timetable(tag = timetableCandidate.tag))
-            saveTimetableCourses(timetableCandidate.courses, timetable)
-            responses.add(
-                TimetableResponse(
-                    timetable.id!!,
-                    timetable.tag.name,
-                    toTimetableCourseResponses(timetableCandidate.courses)
-                )
-            )
-        }
-        return responses
-    }
-
-    private fun saveTimetableCourses(courses: Courses, timetable: Timetable) {
-        for (course in courses.values) {
-            timetableCourseWriter.save(TimetableCourse(timetableId = timetable.id!!, courseId = course.id!!))
-        }
-    }
-
-    private fun toTimetableCourseResponses(courses: Courses) =
-        courses.values.map { TimetableCourseResponse.from(it, courseTimeReader.findAllByCourseId(it.id!!)) }
 }
