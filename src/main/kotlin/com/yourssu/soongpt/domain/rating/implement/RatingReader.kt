3cdac0443c15952560d2761ebbbaaa918c162152
dwl21 <nggus5@gmail.com>
2025-02-07 02:17:48 +0900

    feat: 평점 조회 하여 map형태로 변환

--- src/main/kotlin/com/yourssu/soongpt/domain/rating/implement/RatingReader.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/rating/implement/RatingReader.kt
@@ -1,6 +1,7 @@
 package com.yourssu.soongpt.domain.rating.implement
 
 import com.yourssu.soongpt.domain.course.implement.Courses
+import com.yourssu.soongpt.domain.rating.implement.Rating.Companion.INIT
 import com.yourssu.soongpt.domain.rating.implement.exception.RatingNotFoundByCourseNameException
 import org.springframework.stereotype.Component
 
@@ -8,12 +9,31 @@ import org.springframework.stereotype.Component
 class RatingReader(
     private val ratingRepository: RatingRepository,
 ) {
-    fun findAllBy(courses: Courses): List<Rating> {
-        return courses.unpackNameAndProfessor().mapNotNull { (courseName, professorName) ->
-            ratingRepository.findByCourseNameAndProfessorName(courseName, professorName)
+    fun findAllPointPairs(courses: Courses): Map<Long, Double> {
+        val ratings = findAllBy(courses)
+        val pointPairs = mutableMapOf<Long, Double>()
+        for (course in courses.values) {
+            val find = ratings.find {
+                it.courseName.contains(course.courseName) && it.professorName == (course.professorName?.replace(" 교수님", "") ?: "")
+            }
+            pointPairs[course.id!!] = find?.point ?: INIT
         }
+        return pointPairs
     }
 
+    private fun findAllBy(courses: Courses): List<Rating> {
+        val ratings = mutableListOf<Rating>()
+        for (course in courses.values) {
+            val find = ratingRepository.findByCourseNameAndProfessorName(
+                course.courseName,
+                course.professorName?.replace(" 교수님", "") ?: ""
+            )
+            if (find != null) {
+                ratings.add(find)
+            }
+        }
+        return ratings
+    }
 
     fun findBy(courseName: String, professorName: String): Rating {
         return ratingRepository.findByCourseNameAndProfessorName(courseName, professorName)
