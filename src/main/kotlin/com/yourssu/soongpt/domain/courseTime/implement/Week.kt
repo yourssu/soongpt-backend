589a8465d188e256c40383dab3c25cc029e91faa
chaeyeon0130 <k84292330@gmail.com>
2025-02-06 14:15:31 +0900

    feat: Classification, Week 자체 valueOf() 생성

--- src/main/kotlin/com/yourssu/soongpt/domain/courseTime/implement/Week.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/courseTime/implement/Week.kt
@@ -10,4 +10,19 @@ enum class Week(
     FRIDAY("fri"),
     SATURDAY("sat"),
     SUNDAY("sun");
+
+    companion object {
+        fun fromName(name: String): Week {
+            return when(name) {
+                "월" -> Week.MONDAY
+                "화" -> Week.TUESDAY
+                "수" -> Week.WEDNESDAY
+                "목" -> Week.THURSDAY
+                "금" -> Week.FRIDAY
+                "토" -> Week.SATURDAY
+                "일" -> Week.SUNDAY
+                else -> throw IllegalArgumentException("알 수 없는 요일 : $name")
+            }
+        }
+    }
 }
