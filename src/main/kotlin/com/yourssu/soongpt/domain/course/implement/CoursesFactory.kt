b48780a4c36d06521ff84874df53e10907430336
dwl21 <nggus5@gmail.com>
2025-02-06 19:23:51 +0900

    feat: 시간표 후보들에 과목을 추가하는 코드 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CoursesFactory.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CoursesFactory.kt
@@ -20,4 +20,23 @@ class CoursesFactory(
             throw InvalidTimetableRequestException()
         }
     }
+
+    fun districtDuplicatedCourses(target: List<Courses>): CoursesFactory {
+        return CoursesFactory(coursesCandidates.filter { courses ->
+            target.map { it.unpackNameAndProfessor().first().first != courses.unpackNameAndProfessor().first().first }
+                .reduce(Boolean::and)
+        })
+    }
+
+    fun allCases(): CoursesFactory {
+        return CoursesFactory(coursesCandidates.fold(listOf(emptyList<Course>())) { acc, courses ->
+            acc.flatMap { currentCombination ->
+                acc + courses.values.map { course -> currentCombination + course }
+            }
+        }.map { Courses(it) })
+    }
+
+    fun filterLessThanTotalCredit(standard: Int): List<Courses> {
+        return coursesCandidates.filter { it.totalCredit() < standard }
+    }
 }
