1b724498d7a21654bd8e5a8a6c1d161d28f1402c
dwl21 <nggus5@gmail.com>
2025-02-07 23:48:33 +0900

    feat: 시간표 점수 응답에 보여주기

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/dto/TimetableResponse.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/dto/TimetableResponse.kt
@@ -5,13 +5,15 @@ import com.yourssu.soongpt.domain.timetable.implement.Timetable
 data class TimetableResponse(
     val timetableId: Long,
     val tag: String,
+    var score: Int?,
     val courses: List<TimetableCourseResponse>,
 ) {
     companion object {
-        fun from(timetable: Timetable, courses: List<TimetableCourseResponse>): TimetableResponse {
+        fun from(timetable: Timetable, courses: List<TimetableCourseResponse>, score: Int? = null): TimetableResponse {
             return TimetableResponse(
                 timetableId = timetable.id!!,
                 tag = timetable.tag.name,
+                score = score,
                 courses = courses,
             )
         }
