592b6e6b59fc0f7df4e07e617c73780342510082
dwl21 <nggus5@gmail.com>
2025-02-07 17:45:14 +0900

    예시 엔티티 삭제

--- src/main/kotlin/com/yourssu/soongpt/message/implement/MessageRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.yourssu.soongpt.message.implement
-
-interface MessageRepository {
-    fun save(message: Message): Message
-    fun get(id: Long): Message
-    fun findAll(): List<Message>
-}
