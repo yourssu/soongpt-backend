0ffcca4d413e696d9943d0d3d34dd108d3d4ff92
dwl21 <nggus5@gmail.com>
2025-02-08 16:37:44 +0900

    feat: 요일 표시 이름 변경

--- src/main/kotlin/com/yourssu/soongpt/domain/courseTime/implement/Week.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/courseTime/implement/Week.kt
@@ -3,26 +3,17 @@ package com.yourssu.soongpt.domain.courseTime.implement
 enum class Week(
     val displayName: String,
 ) {
-    MONDAY("mon"),
-    TUESDAY("tue"),
-    WEDNESDAY("wed"),
-    THURSDAY("thu"),
-    FRIDAY("fri"),
-    SATURDAY("sat"),
-    SUNDAY("sun");
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일");
 
     companion object {
         fun fromName(name: String): Week {
-            return when(name) {
-                "월" -> MONDAY
-                "화" -> TUESDAY
-                "수" -> WEDNESDAY
-                "목" -> THURSDAY
-                "금" -> FRIDAY
-                "토" -> SATURDAY
-                "일" -> SUNDAY
-                else -> throw IllegalArgumentException("알 수 없는 요일 : $name")
-            }
+            return entries.find { it.displayName == name } ?: throw IllegalArgumentException("알 수 없는 요일 : $name")
         }
 
         fun weekdays(): List<Week> {
