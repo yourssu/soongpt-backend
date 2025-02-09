7feb2754f92b9ebe5abf64c78c2a901398c401a4
dwl21 <nggus5@gmail.com>
2025-02-09 16:02:06 +0900

    feat: 레이팅 조회 쿼리 개선

--- src/main/kotlin/com/yourssu/soongpt/domain/rating/implement/RatingRepository.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/rating/implement/RatingRepository.kt
@@ -3,4 +3,5 @@ package com.yourssu.soongpt.domain.rating.implement
 interface RatingRepository {
     fun save(rating: Rating): Rating
     fun findByCourseNameAndProfessorName(courseName: String, professorName: String): Rating?
+    fun findAllByCourseNameAndProfessorName(pairs: List<Triple<Long, String, String>>): Map<Long, Double>
 }
--- src/main/kotlin/com/yourssu/soongpt/domain/rating/storage/RatingRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/rating/storage/RatingRepositoryImpl.kt
@@ -18,12 +18,34 @@ class RatingRepositoryImpl(
 
     override fun findByCourseNameAndProfessorName(courseName: String, professorName: String): Rating? {
         return jpaQueryFactory.selectFrom(ratingEntity)
-            .where(ratingEntity.courseName.like(courseName))
+            .where(ratingEntity.courseName.eq(courseName))
             .where(ratingEntity.professorName.eq(professorName))
             .fetchFirst()
             ?.toDomain()
     }
+
+    override fun findAllByCourseNameAndProfessorName(pairs: List<Triple<Long, String, String>>): Map<Long, Double> {
+        val results = mutableMapOf<Long, Double>()
+
+        val predicates = pairs.map {
+            ratingEntity.courseName.eq(it.second).and(ratingEntity.professorName.eq(it.third))
+        }
+
+        val ratings = jpaQueryFactory.selectFrom(ratingEntity)
+            .where(predicates.reduce { acc, predicate -> acc.or(predicate) })
+            .fetch()
+            .map { it.toDomain() }
+
+        for (pair in pairs) {
+            val rating =
+                ratings.find { it.courseName == pair.second && it.professorName == pair.third }
+            results[pair.first] = rating?.point ?: Rating.INIT
+        }
+        return results
+    }
+
+
 }
 
-interface RatingJpaRepository: JpaRepository<RatingEntity, Long> {
+interface RatingJpaRepository : JpaRepository<RatingEntity, Long> {
 }
