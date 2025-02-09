17fcb78c1aed55022e601f3c23c11382ee217804
dwl21 <nggus5@gmail.com>
2025-02-10 01:00:34 +0900

    chore: 비동기 설정

--- src/main/kotlin/com/yourssu/soongpt/SoongptApplication.kt
+++ src/main/kotlin/com/yourssu/soongpt/SoongptApplication.kt
@@ -4,11 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
-import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableAsync
 @ConfigurationPropertiesScan
 class SoongptApplication
 
