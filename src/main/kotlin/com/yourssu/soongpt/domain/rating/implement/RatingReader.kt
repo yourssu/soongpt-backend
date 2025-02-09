e41927bab56cf0ec64089f6b1cb797b11aae2797
dwl21 <nggus5@gmail.com>
2025-02-09 16:02:06 +0900

    feat: 레이팅 조회 쿼리 개선

--- src/main/kotlin/com/yourssu/soongpt/domain/rating/implement/RatingReader.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/rating/implement/RatingReader.kt
@@ -1,8 +1,6 @@
 package com.yourssu.soongpt.domain.rating.implement
 
 import com.yourssu.soongpt.domain.course.implement.Courses
-import com.yourssu.soongpt.domain.rating.implement.Rating.Companion.INIT
-import com.yourssu.soongpt.domain.rating.implement.exception.RatingNotFoundByCourseNameException
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,33 +8,7 @@ class RatingReader(
     private val ratingRepository: RatingRepository,
 ) {
     fun findAllPointPairs(courses: Courses): Map<Long, Double> {
-        val ratings = findAllBy(courses)
-        val pointPairs = mutableMapOf<Long, Double>()
-        for (course in courses.values) {
-            val find = ratings.find {
-                it.courseName == course.courseName && it.professorName == course.professorName
-            }
-            pointPairs[course.id!!] = find?.point ?: INIT
-        }
-        return pointPairs
-    }
-
-    private fun findAllBy(courses: Courses): List<Rating> {
-        val ratings = mutableListOf<Rating>()
-        for (course in courses.values) {
-            val rating = ratingRepository.findByCourseNameAndProfessorName(
-                course.courseName,
-                course.professorName?: ""
-            )
-            if (rating != null) {
-                ratings.add(rating)
-            }
-        }
-        return ratings
-    }
-
-    fun findBy(courseName: String, professorName: String): Rating {
-        return ratingRepository.findByCourseNameAndProfessorName(courseName, professorName)
-            ?: throw RatingNotFoundByCourseNameException()
+        val candidates = courses.values.map { Triple(it.id!!, it.courseName, it.professorName?:"") }
+        return ratingRepository.findAllByCourseNameAndProfessorName(candidates)
     }
 }
