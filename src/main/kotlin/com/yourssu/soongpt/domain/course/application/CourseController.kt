4d317a2257cbc3c0a25c2820053b2d7132d8e805
dwl21 <nggus5@gmail.com>
2025-02-09 21:26:33 +0900

    chore: 모든 API 로깅/ dto json형식 parser

--- src/main/kotlin/com/yourssu/soongpt/domain/course/application/CourseController.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/application/CourseController.kt
@@ -1,5 +1,6 @@
 package com.yourssu.soongpt.domain.course.application
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.yourssu.soongpt.common.business.dto.Response
 import com.yourssu.soongpt.domain.course.application.dto.CreateCourseRequest
 import com.yourssu.soongpt.domain.course.application.dto.GeneralRequiredCourseRequest
@@ -7,11 +8,15 @@ import com.yourssu.soongpt.domain.course.application.dto.MajorElectiveCourseRequ
 import com.yourssu.soongpt.domain.course.application.dto.MajorRequiredCourseRequest
 import com.yourssu.soongpt.domain.course.business.CourseService
 import com.yourssu.soongpt.domain.course.business.dto.CourseResponse
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.swagger.v3.oas.annotations.Hidden
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+
+private val logger = KotlinLogging.logger {}
+private val mapper = ObjectMapper()
 
 @RestController
 @RequestMapping("/api/courses")
@@ -20,19 +25,25 @@ class CourseController(
 ) {
     @GetMapping("/major/required")
     fun getMajorRequiredCourses(@Valid @ModelAttribute request: MajorRequiredCourseRequest): ResponseEntity<Response<List<CourseResponse>>> {
+        logger.info { "GET /api/courses/major/required  request: ${mapper.writeValueAsString(request)}" }
         val response = courseService.findByDepartmentNameInMajorRequired(request.toCommand())
+        logger.info { "GET /api/courses/major/required response: ${mapper.writeValueAsString(response)}" }
         return ResponseEntity.ok().body(Response(result = response))
     }
 
     @GetMapping("/major/elective")
     fun getMajorElectiveCourses(@Valid @ModelAttribute request: MajorElectiveCourseRequest): ResponseEntity<Response<List<CourseResponse>>> {
+        logger.info { "GET /api/courses/major/elective  request: ${mapper.writeValueAsString(request)}" }
         val response = courseService.findByDepartmentNameInMajorElective(request.toCommand())
+        logger.info { "GET /api/courses/major/elective response: ${mapper.writeValueAsString(response)}" }
         return ResponseEntity.ok().body(Response(result = response))
     }
 
     @GetMapping("/general/required")
     fun getGeneralRequiredCourses(@Valid @ModelAttribute request: GeneralRequiredCourseRequest): ResponseEntity<Response<List<CourseResponse>>> {
+        logger.info { "GET /api/courses/general/required  request: ${mapper.writeValueAsString(request)}" }
         val response = courseService.findByDepartmentNameInGeneralRequired(request.toCommand())
+        logger.info { "GET /api/courses/general/required response: ${mapper.writeValueAsString(response)}" }
         return ResponseEntity.ok().body(Response(result = response))
     }
 
