5bfa672ed6b27890c1c82e52c055fb5322d32291
dwl21 <nggus5@gmail.com>
2025-02-13 19:14:59 +0900

    feat: db 접속 문제 500

--- src/main/kotlin/com/yourssu/soongpt/common/handler/InternalServerErrorControllerAdvice.kt
+++ src/main/kotlin/com/yourssu/soongpt/common/handler/InternalServerErrorControllerAdvice.kt
@@ -2,6 +2,8 @@ package com.yourssu.soongpt.common.handler
 
 import com.yourssu.soongpt.common.handler.dto.ErrorResponse
 import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.dao.DataAccessResourceFailureException
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -16,5 +18,17 @@ class InternalServerErrorControllerAdvice {
         logger.error { e }
         return ResponseEntity.internalServerError()
             .body(ErrorResponse.from(InternalServerError()))
+    }
+
+    @ExceptionHandler(DataAccessResourceFailureException::class)
+    fun handleDataAccessException(e: DataAccessResourceFailureException): ResponseEntity<ErrorResponse> {
+        logger.error { e }
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+            .body(
+                ErrorResponse(
+                    status = HttpStatus.SERVICE_UNAVAILABLE.value(),
+                    message = "데이터베이스에 접근할 수 없습니다. {{ ${e.message} }}"
+                )
+            )
     }
 }
