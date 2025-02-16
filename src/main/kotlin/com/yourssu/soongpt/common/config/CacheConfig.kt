dbe7e8bc53d7d812ef5191c443d9aeb136c78a1d
dwl21 <nggus5@gmail.com>
2025-02-16 14:48:27 +0900

    feat: 과목 조회 API 캐싱

---
+++ src/main/kotlin/com/yourssu/soongpt/common/config/CacheConfig.kt
@@ -0,0 +1,23 @@
+package com.yourssu.soongpt.common.config
+
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.concurrent.ConcurrentMapCache
+import org.springframework.cache.support.SimpleCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableCaching
+class CacheConfig {
+    @Bean
+    fun cacheManager(): CacheManager {
+        val cacheManager = SimpleCacheManager()
+        cacheManager.setCaches(
+            listOf(
+                ConcurrentMapCache("courseCache"),
+            )
+        )
+        return cacheManager
+    }
+}
