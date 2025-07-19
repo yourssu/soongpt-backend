663b78495d5e57f0790f24bbb856fda1cb99b296
dwl21 <nggus5@gmail.com>
2025-07-19 12:32:49 +0900

    [#45]; chore: 슬랙 알림 로직 제거

--- src/main/kotlin/com/yourssu/soongpt/SoongptApplication.kt
+++ src/main/kotlin/com/yourssu/soongpt/SoongptApplication.kt
@@ -3,10 +3,8 @@ package com.yourssu.soongpt
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
-@EnableFeignClients
 @ConfigurationPropertiesScan
 class SoongptApplication
 
