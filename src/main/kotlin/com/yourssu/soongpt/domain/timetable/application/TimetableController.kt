3acd764bed487793bb288df4852f18a8b3fcc60b
dwl21 <nggus5@gmail.com>
2025-07-19 12:12:39 +0900

    [#45]; feat: 시간표 생성 알림용 로깅

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/application/TimetableController.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/application/TimetableController.kt
@@ -18,6 +18,7 @@ class TimetableController(
     @PostMapping
     fun createTimetable(@RequestBody request: TimetableCreatedRequest): ResponseEntity<Response<TimetableResponses>> {
         val responses = timetableService.createTimetable(request.toCommand())
+        Notification.notifyTimetableCreated(responses)
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(Response(result = responses))
     }
