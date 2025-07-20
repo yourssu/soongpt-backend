1b0b3fb763f0df34e6c98c340ea7313c5de6a675
dwl21 <nggus5@gmail.com>
2025-07-21 00:02:26 +0900

    [#71]; feat: 검색 API schoolId, department, grade 제거

--- src/main/kotlin/com/yourssu/soongpt/domain/course/application/dto/SearchCoursesRequest.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/application/dto/SearchCoursesRequest.kt
@@ -3,9 +3,6 @@ package com.yourssu.soongpt.domain.course.application.dto
 import com.yourssu.soongpt.domain.course.business.query.SearchCoursesQuery
 
 data class SearchCoursesRequest(
-    val schoolId: Int,
-    val department: String,
-    val grade: Int,
     val q: String = "",
     val page: Int = 0,
     val size: Int = 20,
@@ -13,9 +10,6 @@ data class SearchCoursesRequest(
 ) {
     fun toQuery(): SearchCoursesQuery {
         return SearchCoursesQuery(
-            schoolId = schoolId,
-            department = department,
-            grade = grade,
             query = q,
             page = page,
             size = size,
