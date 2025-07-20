1b07e824c85742ba5ab6abd9f74ba26f4bea639d
dwl21 <nggus5@gmail.com>
2025-07-20 15:01:42 +0900

    [#43]; feat: Course 엔티티 컬럼 재설정

--- src/main/kotlin/com/yourssu/soongpt/domain/course/storage/CourseEntity.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/storage/CourseEntity.kt
@@ -1,6 +1,6 @@
 package com.yourssu.soongpt.domain.course.storage
 
-import com.yourssu.soongpt.domain.course.implement.Classification
+import com.yourssu.soongpt.domain.course.implement.Category
 import com.yourssu.soongpt.domain.course.implement.Course
 import jakarta.persistence.*
 
@@ -12,38 +12,74 @@ class CourseEntity(
     val id: Long? = null,
 
     @Column(nullable = false)
-    val courseName: String,
-
-    @Column(nullable = true, unique = true)
-    val courseCode: Int?,
+    @Enumerated(EnumType.STRING)
+    val category: Category,
 
     @Column(nullable = true)
-    val professorName: String?,
+    val subCategory: String? = null,
 
     @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    val classification: Classification,
+    val field: String? = null,
+
+    @Column(nullable = true, unique = true)
+    val code: Int,
 
     @Column(nullable = false)
-    val credit: Int,
+    val name: String,
+
+    @Column(nullable = true)
+    val professor: String? = null,
+
+    @Column(nullable = false)
+    val department: String,
+
+    @Column(nullable = false)
+    val timePoints: String,
+
+    @Column(nullable = false)
+    val personeel: Int,
+
+    @Column(nullable = false)
+    val scheduleRoom: String,
+
+    @Column(nullable = false)
+    val target: String,
 ) {
     companion object {
-        fun from(course: Course) = CourseEntity(
-            id = course.id,
-            courseName = course.courseName,
-            courseCode = course.courseCode,
-            professorName = course.professorName,
-            classification = course.classification,
-            credit = course.credit,
-        )
+        fun from(course: Course): CourseEntity {
+
+            return CourseEntity(
+                id = course.id,
+                category = course.category,
+                subCategory = course.subCategory,
+                field = course.field,
+                code = course.code,
+                name = course.name,
+                professor = course.professor,
+                department = course.department,
+                timePoints = course.timePoints,
+                personeel = 0,
+                scheduleRoom = course.scheduleRoom,
+                target = course.target,
+            )
+        }
     }
 
-    fun toDomain() = Course(
-        id = id,
-        courseName = courseName,
-        professorName = professorName,
-        classification = classification,
-        credit = credit,
-        courseCode = courseCode,
-    )
+
+    fun to(): Course {
+        return Course(
+            id = id,
+            category = category,
+            subCategory = subCategory,
+            field = field,
+            code = code,
+            name = name,
+            professor = professor,
+            department = department,
+            timePoints = timePoints,
+            personeel = personeel,
+            scheduleRoom = scheduleRoom,
+            target = target
+        )
+    }
 }
