16b2b36c847d43675626b9614419be7fe2fd7fd9
dwl21 <nggus5@gmail.com>
2025-07-20 15:01:42 +0900

    [#43]; feat: Course 엔티티 컬럼 재설정

--- src/main/kotlin/com/yourssu/soongpt/domain/course/implement/Course.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/implement/Course.kt
@@ -2,10 +2,16 @@ package com.yourssu.soongpt.domain.course.implement
 
 class Course(
     val id: Long? = null,
-    val courseName: String,
-    val courseCode: Int? = null,
-    val professorName: String? = null,
-    val classification: Classification,
-    val credit: Int = 0,
-) {
+    val category: Category,
+    val subCategory: String? = null,
+    val field: String? = null,
+    val code: Int,
+    val name: String,
+    val professor: String? = null,
+    val department: String,
+    val timePoints: String,
+    val personeel: Int,
+    val scheduleRoom: String,
+    val target: String,
+){
 }
