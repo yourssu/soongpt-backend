eda0107878ee1a6ea9cedef36ee0aac8754ab618
dwl21 <nggus5@gmail.com>
2025-07-20 19:54:56 +0900

    [#43]; feat: department 미사용 조회 로직 제거

--- src/main/kotlin/com/yourssu/soongpt/domain/department/implement/DepartmentRepository.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/department/implement/DepartmentRepository.kt
@@ -1,8 +1,6 @@
 package com.yourssu.soongpt.domain.department.implement
 
 interface DepartmentRepository {
-    fun save(department: Department): Department
     fun saveAll(departments: List<Department>): List<Department>
-    fun findByName(name: String): Department?
-    fun findAll(): List<Department>
+    fun getByName(name: String): Department
 }
