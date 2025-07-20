313cb98440812340f4562fd24c4cffbb0d713f93
dwl21 <nggus5@gmail.com>
2025-07-20 15:17:17 +0900

    [#43]; feat: Target 엔티티 재설계

--- src/main/kotlin/com/yourssu/soongpt/domain/target/implement/TargetRepository.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/target/implement/TargetRepository.kt
@@ -1,7 +1,5 @@
 package com.yourssu.soongpt.domain.target.implement
 
 interface TargetRepository {
-    fun save(target: Target): Target
-    fun findAllByCourseId(courseId: Long): List<Target>
-    fun findAllByCourseIdToDisplayName(courseId: Long): List<String>
+    fun findAllByCode(code: Long): List<Target>
 }
