2a0cb2f596bf8d2646e2fcebf17518ff65314945
chlee <chlee@student.42seoul.kr>
2025-07-22 00:03:53 +0900

    [#84]; feat: scheduleRoom 필드 파싱을 위한 일급 컬렉션 도입

--- src/main/kotlin/com/yourssu/soongpt/domain/courseTime/implement/CourseTime.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/courseTime/implement/CourseTime.kt
@@ -3,7 +3,6 @@ package com.yourssu.soongpt.domain.courseTime.implement
 import com.yourssu.soongpt.domain.courseTime.implement.exception.InvalidCourseTimeException
 
 class CourseTime(
-    val id: Long? = null,
     val week: Week,
     val startTime: Time,
     val endTime: Time,
