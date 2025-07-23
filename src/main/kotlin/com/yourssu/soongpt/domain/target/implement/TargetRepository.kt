ff8615e445a19235d38b76f4877f7ae12ee1adb9
dwl21 <nggus5@gmail.com>
2025-07-24 01:26:23 +0900

    [#90]; feat: 모든 분반에 대한 과목 조회 메서드 findAllByClass

--- src/main/kotlin/com/yourssu/soongpt/domain/target/implement/TargetRepository.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/target/implement/TargetRepository.kt
@@ -3,4 +3,5 @@ package com.yourssu.soongpt.domain.target.implement
 interface TargetRepository {
     fun findAllByCode(code: Long): List<Target>
     fun findAllByDepartmentGrade(departmentId: Long, grade: Int): List<Target>
+    fun findAllByClass(departmentId: Long, code: Long, grade: Int): List<Target>
 }
