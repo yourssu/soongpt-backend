88c364ff2918ce2ae4f36a303e52f4b952e7c56a
dwl21 <nggus5@gmail.com>
2025-07-24 01:26:23 +0900

    [#90]; feat: 모든 분반에 대한 과목 조회 메서드 findAllByClass
    (cherry picked from commit ff8615e445a19235d38b76f4877f7ae12ee1adb9)

--- src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseRepository.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseRepository.kt
@@ -1,10 +1,7 @@
 package com.yourssu.soongpt.domain.course.implement
 
-import com.yourssu.soongpt.domain.course.implement.dto.GroupedCoursesByCategoryDto
-
 interface CourseRepository {
     fun get(code: Long): Course
-    fun getAll(codes: List<Long>): List<Course>
-    fun getAllInCategory(category: Category, courseIds: List<Long>): List<Course>
-    fun groupByCategory(codes: List<Long>): GroupedCoursesByCategoryDto
+    fun findAll(courseIds: List<Long>): List<Course>
+    fun findAllInCategory(category: Category, courseIds: List<Long>): List<Course>
 }
