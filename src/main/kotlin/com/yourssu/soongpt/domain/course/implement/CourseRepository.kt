79217c0322fcf32c941c7e9f2b6ce47977d0ffa4
dwl21 <nggus5@gmail.com>
2025-07-24 00:37:04 +0900

    [#90]; fix: 과목 조회 타겟, 과목 조회 순으로 구현
    (cherry picked from commit 2cedb13bc10bc004446c87218b18a7eac2173be9)

--- src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseRepository.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseRepository.kt
@@ -2,6 +2,5 @@ package com.yourssu.soongpt.domain.course.implement
 
 interface CourseRepository {
     fun get(code: Long): Course
-    fun findAll(courseIds: List<Long>): List<Course>
-    fun findAllInCategory(category: Category, courseIds: List<Long>): List<Course>
+    fun findAllByCategoryTarget(category: Category, courseIds: List<Long>): List<Course>
 }
