e5367782bfed72b39d9fa640b0d63db1ee3ba5b1
dwl21 <nggus5@gmail.com>
2025-07-24 00:37:04 +0900

    [#90]; fix: 과목 조회 타겟, 과목 조회 순으로 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/course/business/CourseService.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/business/CourseService.kt
@@ -6,6 +6,7 @@ import com.yourssu.soongpt.domain.course.business.dto.MajorRequiredResponse
 import com.yourssu.soongpt.domain.course.business.query.GeneralRequiredCourseQuery
 import com.yourssu.soongpt.domain.course.business.query.MajorElectiveCourseQuery
 import com.yourssu.soongpt.domain.course.business.query.MajorRequiredCourseQuery
+import com.yourssu.soongpt.domain.course.implement.Category
 import com.yourssu.soongpt.domain.course.implement.CourseReader
 import com.yourssu.soongpt.domain.department.implement.DepartmentReader
 import com.yourssu.soongpt.domain.target.implement.TargetReader
@@ -19,22 +20,22 @@ class CourseService(
 ) {
     fun findAll(query: MajorRequiredCourseQuery): List<MajorRequiredResponse> {
         val department = departmentReader.getByName(query.departmentName)
-        val target = targetReader.getByDepartmentGrade(department, query.grade)
-        val courses = courseReader.findAllByTargetInMajorRequired(target)
+        val targets = targetReader.findAllByDepartmentGrade(department, query.grade)
+        val courses = courseReader.findAllInCategory(Category.MAJOR_REQUIRED, targets.map { it.courseId })
         return courses.map { MajorRequiredResponse.from(it) }
     }
 
     fun findAll(query: MajorElectiveCourseQuery): List<MajorElectiveResponse> {
         val department = departmentReader.getByName(query.departmentName)
-        val target = targetReader.getByDepartmentGrade(department, query.grade)
-        val courses = courseReader.findAllByTargetInMajorElective(target)
+        val targets = targetReader.findAllByDepartmentGrade(department, query.grade)
+        val courses = courseReader.findAllInCategory(Category.MAJOR_ELECTIVE, targets.map { it.courseId })
         return courses.map { MajorElectiveResponse.from(it) }
     }
 
     fun findAll(query: GeneralRequiredCourseQuery): List<GeneralRequiredResponse> {
         val department = departmentReader.getByName(query.departmentName)
-        val target = targetReader.getByDepartmentGrade(department, query.grade)
-        val courses = courseReader.findAllByTargetInGeneralRequired(target)
+        val targets = targetReader.findAllByDepartmentGrade(department, query.grade)
+        val courses = courseReader.findAllInCategory(Category.GENERAL_REQUIRED, targets.map { it.courseId })
         return courses.map { GeneralRequiredResponse.from(it) }
     }
 }
--- src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseReader.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseReader.kt
@@ -1,21 +1,12 @@
 package com.yourssu.soongpt.domain.course.implement
 
-import com.yourssu.soongpt.domain.target.implement.Target
 import org.springframework.stereotype.Component
 
 @Component
 class CourseReader(
     private val courseRepository: CourseRepository,
-)  {
-    fun findAllByTargetInMajorRequired(target: Target): List<Course> {
-        return courseRepository.findAllByCategoryTarget(Category.MAJOR_REQUIRED, target)
-    }
-
-    fun findAllByTargetInMajorElective(target: Target): List<Course> {
-        return courseRepository.findAllByCategoryTarget(Category.MAJOR_ELECTIVE, target)
-    }
-
-    fun findAllByTargetInGeneralRequired(target: Target): List<Course> {
-        return courseRepository.findAllByCategoryTarget(Category.GENERAL_REQUIRED, target)
+) {
+    fun findAllInCategory(category: Category, courseIds: List<Long>): List<Course> {
+        return courseRepository.findAllByCategoryTarget(category, courseIds)
     }
 }
