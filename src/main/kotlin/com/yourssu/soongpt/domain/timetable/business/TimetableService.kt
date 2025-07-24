27a55d2b03ffe4b6fba470869bdc47a39c8df607
chlee <chlee@student.42seoul.kr>
2025-07-24 15:36:22 +0900

    [94]; feat: TimetableService 비즈니스 로직 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/TimetableService.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/TimetableService.kt
@@ -1,24 +1,36 @@
 package com.yourssu.soongpt.domain.timetable.business
 
+import com.yourssu.soongpt.domain.courseTime.implement.CourseTime
+import com.yourssu.soongpt.domain.courseTime.implement.CourseTimes
+import com.yourssu.soongpt.domain.timetable.business.dto.TimetableCourseResponse
 import com.yourssu.soongpt.domain.timetable.business.dto.TimetableCreatedCommand
 import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponse
 import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponses
+import com.yourssu.soongpt.domain.timetable.implement.TimetableCourseReader
+import com.yourssu.soongpt.domain.timetable.implement.TimetableGenerator
+import com.yourssu.soongpt.domain.timetable.implement.TimetableReader
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class TimetableService(
+    private val timetableReader: TimetableReader,
+    private val timetableGenerator: TimetableGenerator,
+    private val timetableCourseReader: TimetableCourseReader,
 ) {
     @Transactional
     fun createTimetable(command: TimetableCreatedCommand): TimetableResponses {
-        return TimetableResponses(listOf())
+        val timetables = timetableGenerator.generate(command)
+        return timetables
     }
 
     fun getTimeTable(id: Long): TimetableResponse {
-        return TimetableResponse(
-            timetableId = id,
-            tag = "Sample Tag",
-            courses = listOf()
-        )
+        val timetable = timetableReader.get(id)
+        val courses = timetableCourseReader.findAllCourseByTimetableId(id)
+        val coursesWithTime = courses.map { course ->
+            val courseTimes = CourseTimes.from(course.scheduleRoom).toList();
+            TimetableCourseResponse.from(course, courseTimes)
+        }
+        return TimetableResponse.from(timetable, coursesWithTime)
     }
 }
