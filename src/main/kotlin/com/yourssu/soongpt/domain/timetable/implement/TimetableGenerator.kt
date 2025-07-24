361279eee9802a7573ab8c2b528e0676326bef0c
chlee <chlee@student.42seoul.kr>
2025-07-24 15:37:16 +0900

    [94]; feat: TimetableGenerator

---
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimetableGenerator.kt
@@ -0,0 +1,16 @@
+package com.yourssu.soongpt.domain.timetable.implement
+
+import com.yourssu.soongpt.domain.timetable.business.dto.TimetableCreatedCommand
+import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponse
+import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponses
+import org.springframework.stereotype.Component
+
+@Component
+class TimetableGenerator (
+){
+
+    fun generate(command: TimetableCreatedCommand): TimetableResponses {
+        return TimetableResponses(listOf())
+    }
+
+}
