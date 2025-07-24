47bd71e6f2c2f529bd1866a2cb374a5cf207426c
chlee <chlee@student.42seoul.kr>
2025-07-24 15:38:42 +0900

    [94]; chore: optimize imports

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimetableGenerator.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimetableGenerator.kt
@@ -1,7 +1,6 @@
 package com.yourssu.soongpt.domain.timetable.implement
 
 import com.yourssu.soongpt.domain.timetable.business.dto.TimetableCreatedCommand
-import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponse
 import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponses
 import org.springframework.stereotype.Component
 
