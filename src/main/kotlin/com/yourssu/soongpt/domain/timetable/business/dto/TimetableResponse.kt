70e8208beb16ca71da3ced900d2ac84e86f2507f
chlee <chlee@student.42seoul.kr>
2025-07-24 15:34:46 +0900

    [94]; feat: score field 반영

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/dto/TimetableResponse.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/dto/TimetableResponse.kt
@@ -1,21 +1,23 @@
 package com.yourssu.soongpt.domain.timetable.business.dto
 
+import com.yourssu.soongpt.domain.timetable.implement.Timetable
+
 data class TimetableResponse(
     val timetableId: Long,
     val tag: String,
-    val score: Int = 0,
+    val score: Int? = 0,
     val totalPoint: Double = 0.0,
     val courses: List<TimetableCourseResponse>,
 ) {
     companion object {
-//        fun from(timetable: Timetable, courses: List<TimetableCourseResponse>): TimetableResponse {
-//            return TimetableResponse(
-//                timetableId = timetable.timetableId,
-//                tag = timetable.tag,
-//                score = timetable.score,
-//                totalPoint = timetable.totalPoint,
-//                courses = courses,
-//            )
-//        }
+        fun from(timetable: Timetable, courses: List<TimetableCourseResponse>): TimetableResponse {
+            return TimetableResponse(
+                timetableId = timetable.id!!,
+                tag = timetable.tag.description,
+                score = timetable.score,
+                totalPoint = courses.sumOf { it.point.toDouble() },
+                courses = courses,
+            )
+        }
     }
 }
