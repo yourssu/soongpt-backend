a69ef886e1453ba9e7c780d89bf03c0d8e16c6ba
chlee <chlee@student.42seoul.kr>
2025-07-24 15:26:01 +0900

    [94]; feat: TimetableRepository, Reader, Writer 시간표 조회, 생성, 삭제 쿼리 interface, Reader/Writer 구현

---
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/TimetableRepository.kt
@@ -0,0 +1,7 @@
+package com.yourssu.soongpt.domain.timetable.implement
+
+interface TimetableRepository {
+    fun save(timetable: Timetable): Timetable
+    fun get(id: Long): Timetable
+    fun delete(id: Long)
+}
