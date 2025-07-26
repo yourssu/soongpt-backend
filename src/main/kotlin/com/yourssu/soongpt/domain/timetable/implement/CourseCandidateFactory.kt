e42096355a0d9def20afe1037c5e5c85c3af858a
chlee <chlee@student.42seoul.kr>
2025-07-26 18:09:10 +0900

    [92]; feat: CourseCandidate 생성을 위한 Factory 구현
    시간을 5분 단위로 압축한 BitSet을 생성해 CourseCandidate 생성

---
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/CourseCandidateFactory.kt
@@ -0,0 +1,44 @@
+package com.yourssu.soongpt.domain.timetable.implement
+
+import com.yourssu.soongpt.domain.course.implement.Course
+import com.yourssu.soongpt.domain.courseTime.implement.CourseTimes
+import com.yourssu.soongpt.domain.timetable.implement.dto.CompressTime
+import com.yourssu.soongpt.domain.timetable.implement.dto.CompressTimes
+import com.yourssu.soongpt.domain.timetable.implement.dto.CourseCandidate
+import java.util.*
+
+class CourseCandidateFactory {
+    companion object {
+        private const val WHOLE_TIMESLOT = 24 * 7 * 60
+        private const val COMPRESS_TIME_UNIT = 5
+        private const val TIMESLOT_SIZE = WHOLE_TIMESLOT / COMPRESS_TIME_UNIT
+    }
+
+    fun create(course: Course): CourseCandidate {
+        val courseTimes = CourseTimes.from(course.scheduleRoom)
+        val compressTimes = compressTimes(courseTimes)
+        val timeSlot = generateTimeSlot(compressTimes)
+        return CourseCandidate.from(course.code, timeSlot)
+    }
+
+    private fun compressTimes(
+        courseTimes: CourseTimes
+    ): CompressTimes {
+        val compressedTimes = courseTimes.toList().map { courseTime ->
+            CompressTime.from(courseTime)
+        }
+        return CompressTimes.from(compressedTimes)
+    }
+
+    private fun generateTimeSlot(
+        compressTimes: CompressTimes
+    ): BitSet {
+        val timeSlot = BitSet(TIMESLOT_SIZE)
+        for (time in compressTimes.times) {
+            val startIndex = time.compressedStartTime
+            val endIndex = time.compressedEndTime
+            timeSlot.set(startIndex, endIndex)
+        }
+        return timeSlot
+    }
+}
