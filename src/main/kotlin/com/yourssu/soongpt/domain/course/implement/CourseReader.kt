6d1901534c6041bef2575324b578be2863bc17a8
dwl21 <nggus5@gmail.com>
2025-07-26 15:31:13 +0900

    [#96]; feat: findAllByClass

--- src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseReader.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseReader.kt
@@ -1,14 +1,23 @@
 package com.yourssu.soongpt.domain.course.implement
 
 import com.yourssu.soongpt.domain.course.implement.dto.GroupedCoursesByCategoryDto
+import com.yourssu.soongpt.domain.department.implement.Department
+import com.yourssu.soongpt.domain.target.implement.TargetRepository
 import org.springframework.stereotype.Component
 
 @Component
 class CourseReader(
     private val courseRepository: CourseRepository,
+    private val targetRepository: TargetRepository,
+
 ) {
+    fun findAllByClass(department: Department, code: Long, grade: Int): List<Course> {
+        val targets = targetRepository.findAllByClass(department.id!!, code, grade)
+        return courseRepository.findAllById(targets.map { it.courseId })
+    }
+
     fun findAllInCategory(category: Category, courseIds: List<Long>): List<Course> {
-        return courseRepository.findAllByCategory(category, courseIds)
+        return courseRepository.findAllInCategory(category, courseIds)
     }
 
     fun groupByCategory(codes: List<Long>): GroupedCoursesByCategoryDto {
--- src/main/kotlin/com/yourssu/soongpt/domain/target/storage/TargetRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/target/storage/TargetRepositoryImpl.kt
@@ -1,11 +1,14 @@
 package com.yourssu.soongpt.domain.target.storage
 
 import com.querydsl.jpa.impl.JPAQueryFactory
+import com.yourssu.soongpt.domain.course.storage.QCourseEntity.courseEntity
 import com.yourssu.soongpt.domain.target.implement.Target
 import com.yourssu.soongpt.domain.target.implement.TargetRepository
 import com.yourssu.soongpt.domain.target.storage.QTargetEntity.targetEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
+
+private const val DIVISION_DIVISOR = 100
 
 @Component
 class TargetRepositoryImpl (
@@ -24,6 +27,21 @@ class TargetRepositoryImpl (
             .selectFrom(targetEntity)
             .where(
                 targetEntity.departmentId.eq(departmentId),
+                targetEntity.grade.eq(grade)
+            )
+            .fetch()
+            .map { it.toDomain() }
+    }
+
+    override fun findAllByClass(departmentId: Long, code: Long, grade: Int): List<Target> {
+        val codeWithoutDivision = code.div(DIVISION_DIVISOR)
+        return jpaQueryFactory
+            .selectFrom(targetEntity)
+            .innerJoin(courseEntity)
+            .on(targetEntity.courseId.eq(courseEntity.id))
+            .where(
+                targetEntity.departmentId.eq(departmentId),
+                courseEntity.code.divide(DIVISION_DIVISOR).eq(codeWithoutDivision),
                 targetEntity.grade.eq(grade)
             )
             .fetch()
