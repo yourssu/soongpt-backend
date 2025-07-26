a456d68f170bff2b3abdd5521c2497973f23e84f
dwl21 <nggus5@gmail.com>
2025-07-26 14:35:45 +0900

    [#96]; feat: Course 카테고리 별 그룹 메서드 구현

--- src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseRepository.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/implement/CourseRepository.kt
@@ -1,7 +1,10 @@
 package com.yourssu.soongpt.domain.course.implement
 
+import com.yourssu.soongpt.domain.course.implement.dto.GroupedCoursesByCategoryDto
+
 interface CourseRepository {
     fun get(code: Long): Course
     fun getAll(codes: List<Long>): List<Course>
     fun getAllInCategory(category: Category, courseIds: List<Long>): List<Course>
+    fun groupByCategory(codes: List<Long>): GroupedCoursesByCategoryDto
 }
--- src/main/kotlin/com/yourssu/soongpt/domain/course/storage/CourseRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/storage/CourseRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory
 import com.yourssu.soongpt.domain.course.implement.Category
 import com.yourssu.soongpt.domain.course.implement.Course
 import com.yourssu.soongpt.domain.course.implement.CourseRepository
+import com.yourssu.soongpt.domain.course.implement.dto.GroupedCoursesByCategoryDto
 import com.yourssu.soongpt.domain.course.storage.QCourseEntity.courseEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
@@ -32,6 +33,20 @@ class CourseRepositoryImpl(
             )
             .fetch()
             .map { it.toDomain() }
+    }
+
+    override fun groupByCategory(codes: List<Long>): GroupedCoursesByCategoryDto {
+        val categories = listOf(Category.MAJOR_REQUIRED, Category.MAJOR_ELECTIVE, Category.GENERAL_REQUIRED,
+            Category.GENERAL_ELECTIVE)
+        val groupedCourses = categories.associateWith { category ->
+            getAll(codes).filter { it.category == category }
+        }
+        return GroupedCoursesByCategoryDto.from(
+            majorRequiredCourses = groupedCourses[Category.MAJOR_REQUIRED]?: emptyList(),
+            majorElectiveCourses = groupedCourses[Category.MAJOR_ELECTIVE]?: emptyList(),
+            generalRequiredCourses = groupedCourses[Category.GENERAL_REQUIRED]?: emptyList(),
+            generalElectiveCourses = groupedCourses[Category.GENERAL_ELECTIVE]?: emptyList(),
+        )
     }
 }
 
