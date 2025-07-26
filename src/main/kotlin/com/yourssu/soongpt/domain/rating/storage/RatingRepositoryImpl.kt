5b5214cd4546b22e6343ae5b81f09f71767c7831
chlee <chlee@student.42seoul.kr>
2025-07-26 15:42:55 +0900

    [97]; feat: rating 도메인 생성

--- src/main/kotlin/com/yourssu/soongpt/domain/rating/storage/RatingRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/rating/storage/RatingRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package com.yourssu.soongpt.domain.rating.storage
 
-import com.yourssu.soongpt.domain.department.storage.DepartmentEntity
+import com.querydsl.jpa.impl.JPAQueryFactory
+import com.yourssu.soongpt.domain.rating.implement.Rating
+import com.yourssu.soongpt.domain.rating.implement.RatingRepository
+import com.yourssu.soongpt.domain.rating.storage.QRatingEntity.ratingEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
@@ -8,17 +11,25 @@ import org.springframework.stereotype.Repository
 @Repository
 class RatingRepositoryImpl (
     private val ratingJpaRepository: RatingJpaRepository,
+    private val jpaQueryFactory: JPAQueryFactory,
 ) : RatingRepository {
-    override fun saveAll(ratings: List<Rating>): List<Rating> {
-        return ratingJpaRepository.saveAll(ratings.map { RatingEntity.from(it) })
-            .map { it.toDomain() }.toList()
+    override fun save(rating: Rating): Rating {
+        return ratingJpaRepository.save(RatingEntity.from(rating)).toDomain()
     }
 
-    override fun getByCode(code: Long): Rating {
-        return ratingJpaRepository.findByCode(code)?.toDomain()
-            ?: throw RatingNotFoundException()
+    override fun findByCode(code: Long): Rating? {
+        return jpaQueryFactory.selectFrom(ratingEntity)
+            .where(ratingEntity.code.eq(code))
+            .fetchFirst()
+            ?.toDomain()
     }
-)
 
-interface DepartmentJpaRepository : JpaRepository<DepartmentEntity, Long> {
+    override fun findAll(): List<Rating> {
+        return jpaQueryFactory.selectFrom(ratingEntity)
+            .fetch()
+            .map { it.toDomain() }
+    }
+}
+
+interface RatingJpaRepository : JpaRepository<RatingEntity, Long> {
 }
