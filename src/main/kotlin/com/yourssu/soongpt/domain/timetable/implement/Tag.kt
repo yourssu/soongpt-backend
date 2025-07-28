f0db1cf47e9129edc4cb50f56ad8c0b6e9b1d8f8
chlee <chlee@student.42seoul.kr>
2025-07-28 11:25:19 +0900

    [#111]; feat: TagStrategy 필드 추가

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/Tag.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/Tag.kt
@@ -1,15 +1,13 @@
 package com.yourssu.soongpt.domain.timetable.implement
 
-enum class Tag (val description: String) {
-    DEFAULT("기본 태그"),
-    NO_MORNING_CLASSES("아침 수업이 없는 시간표"),
-    HAS_FREE_DAY("공강이 있는 시간표"),
-    NO_LONG_BREAKS("우주 공강이 없는 시간표"),
-    GUARANTEED_LUNCH_TIME("점심시간 보장되는 시간표"),
-    NO_EVENING_CLASSES("저녁수업이 없는 시간표"),
-    ;
+import com.yourssu.soongpt.domain.timetable.implement.strategy.*
 
-    override fun toString(): String {
-        return description
-    }
+enum class Tag (val description: String, val strategy: TagStrategy) {
+    DEFAULT("기본 태그", DefaultStrategy()),
+    NO_MORNING_CLASSES("아침 수업이 없는 시간표", NoMorningClassesStrategy()),
+    HAS_FREE_DAY("공강 날이 있는 시간표", FreeDayTagStrategy()),
+    NO_LONG_BREAKS("우주 공강이 없는 시간표", NoLongBreaksStrategy()),
+    GUARANTEED_LUNCH_TIME("점심시간 보장되는 시간표", GuaranteedLunchTimeStrategy()),
+    NO_EVENING_CLASSES("저녁수업이 없는 시간표", NoEveningClassesStrategy()),
+    ;
 }
