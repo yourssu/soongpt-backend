9997b8cf2c6493a647c26b703ba6e3f70f67a061
chlee <chlee@student.42seoul.kr>
2025-07-28 11:18:15 +0900

    [#111]; fix: omitted day range in calculating compress time

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/dto/CompressTime.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/dto/CompressTime.kt
@@ -1,7 +1,10 @@
 package com.yourssu.soongpt.domain.timetable.implement.dto
 
 import com.yourssu.soongpt.domain.courseTime.implement.CourseTime
-import com.yourssu.soongpt.domain.timetable.implement.timeslot.TIMESLOT_UNIT_MINUTES
+import com.yourssu.soongpt.domain.courseTime.implement.Time
+import com.yourssu.soongpt.domain.courseTime.implement.Week
+import com.yourssu.soongpt.domain.timetable.implement.constant.TIMESLOT_DAY_RANGE
+import com.yourssu.soongpt.domain.timetable.implement.constant.TIMESLOT_UNIT_MINUTES
 
 data class CompressTime(
     val compressedStartTime: Int,
@@ -9,9 +12,25 @@ data class CompressTime(
 ) {
     companion object {
         fun from(courseTime: CourseTime): CompressTime {
-            val start = courseTime.startTime.time / TIMESLOT_UNIT_MINUTES
-            val end = courseTime.endTime.time / TIMESLOT_UNIT_MINUTES
-            return CompressTime(start, end)
+
+            if (courseTime.week == Week.UNKNOWN) {
+                return CompressTime(
+                    compressedStartTime = 0,
+                    compressedEndTime = 0,
+                )
+            } else {
+                val day = courseTime.week.ordinal * TIMESLOT_DAY_RANGE
+                val start = day + courseTime.startTime.time / TIMESLOT_UNIT_MINUTES
+                val end = day + courseTime.endTime.time / TIMESLOT_UNIT_MINUTES
+                return CompressTime(start, end)
+            }
+        }
+
+        fun from(startTime: Time, endTime: Time): CompressTime {
+            return CompressTime(
+                compressedStartTime = startTime.time / TIMESLOT_UNIT_MINUTES,
+                compressedEndTime = endTime.time / TIMESLOT_UNIT_MINUTES
+            )
         }
     }
 }
