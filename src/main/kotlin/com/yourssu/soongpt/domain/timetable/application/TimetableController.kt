3c29283551821937138223af49691aaac18eadaa
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-09-01 03:26:35 +0900

    [#152]; feat: controller&service에 실험실기능추가
    - TimetableController GET /api/timetables 오버로드 추가
    
    - TimetableService getRandomTimetable() 메서드 추가

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/application/TimetableController.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/application/TimetableController.kt
@@ -4,8 +4,9 @@ import com.yourssu.soongpt.common.business.dto.Response
 import com.yourssu.soongpt.common.infrastructure.notification.Notification
 import com.yourssu.soongpt.domain.timetable.application.dto.TimetableCreatedRequest
 import com.yourssu.soongpt.domain.timetable.business.TimetableService
-import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponse
+import com.yourssu.soongpt.domain.timetable.business.dto.LabTimetableResponse
 import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponses
+import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -28,6 +29,13 @@ class TimetableController(
     @GetMapping("/{id}")
     fun getTimetable(@PathVariable id: Long): ResponseEntity<Response<TimetableResponse>> {
         val response = timetableService.getTimeTable(id)
+        return ResponseEntity.ok(Response(result = response))
+    }
+
+    // 웹팀과제를 위한 기능
+    @GetMapping
+    fun getRandomTimetable(): ResponseEntity<Response<LabTimetableResponse>> {
+        val response = timetableService.getRandomTimetable()
         return ResponseEntity.ok(Response(result = response))
     }
 }
