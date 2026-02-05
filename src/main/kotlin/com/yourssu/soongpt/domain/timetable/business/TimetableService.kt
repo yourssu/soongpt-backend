549cca697ca2721c8b8fe4240807b3fad9a43b5c
chlee <chlee@student.42seoul.kr>
2026-02-05 15:30:53 +0900

    fix: conflict merge with stash file (first recommandation timetable logic)

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/TimetableService.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/TimetableService.kt
@@ -3,26 +3,19 @@ package com.yourssu.soongpt.domain.timetable.business
 import com.yourssu.soongpt.common.infrastructure.dto.TimetableCreatedAlarmRequest
 import com.yourssu.soongpt.domain.courseTime.implement.CourseTimes
 import com.yourssu.soongpt.domain.timetable.application.dto.TimetableCreatedRequest
-import com.yourssu.soongpt.domain.timetable.business.dto.TimetableCourseResponse
-import com.yourssu.soongpt.domain.timetable.business.dto.TimetableCreatedCommand
-import com.yourssu.soongpt.domain.timetable.business.dto.LabTimetableResponse
-import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponse
-import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponses
-import com.yourssu.soongpt.domain.timetable.implement.TimetableCourseReader
-import com.yourssu.soongpt.domain.timetable.implement.TimetableGenerator
-import com.yourssu.soongpt.domain.timetable.implement.TimetableReader
-import com.yourssu.soongpt.domain.timetable.implement.LabTimetableMapper
-import com.yourssu.soongpt.domain.timetable.implement.LabTimetableReader
+import com.yourssu.soongpt.domain.timetable.business.dto.*
+import com.yourssu.soongpt.domain.timetable.implement.*
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class TimetableService(
-    private val timetableReader: TimetableReader,
-    private val timetableGenerator: TimetableGenerator,
-    private val timetableCourseReader: TimetableCourseReader,
-    private val labTimetableMapper: LabTimetableMapper,
-    private val labTimetableReader: LabTimetableReader,
+        private val timetableReader: TimetableReader,
+        private val timetableGenerator: TimetableGenerator,
+        private val timetableCourseReader: TimetableCourseReader,
+        private val labTimetableMapper: LabTimetableMapper,
+        private val labTimetableReader: LabTimetableReader,
+        private val timetableRecommendationFacade: TimetableRecommendationFacade
 ) {
     @Transactional
     fun createTimetable(command: TimetableCreatedCommand): TimetableResponses {
@@ -31,13 +24,20 @@ class TimetableService(
         return TimetableResponses(responses)
     }
 
+    fun getFullRecommendation(
+            command: PrimaryTimetableCommand
+    ): FullTimetableRecommendationResponse {
+        return timetableRecommendationFacade.recommend(command)
+    }
+
     fun getTimeTable(id: Long): TimetableResponse {
         val timetable = timetableReader.get(id)
         val courses = timetableCourseReader.findAllCourseByTimetableId(id)
-        val coursesWithTime = courses.map { course ->
-            val courseTimes = CourseTimes.from(course.scheduleRoom).toList();
-            TimetableCourseResponse.from(course, courseTimes)
-        }
+        val coursesWithTime =
+                courses.map { course ->
+                    val courseTimes = CourseTimes.from(course.scheduleRoom).toList()
+                    TimetableCourseResponse.from(course, courseTimes)
+                }
         return TimetableResponse.from(timetable, coursesWithTime)
     }
 
@@ -47,14 +47,15 @@ class TimetableService(
     }
 
     fun createTimetableAlarmRequest(
-        request: TimetableCreatedRequest,
-        responses: TimetableResponses
+            request: TimetableCreatedRequest,
+            responses: TimetableResponses
     ): TimetableCreatedAlarmRequest {
-        val response = TimetableCreatedAlarmRequest(
-            schoolId = request.schoolId,
-            departmentName = request.department,
-            times = responses.timetables.last().timetableId
-        )
+        val response =
+                TimetableCreatedAlarmRequest(
+                        schoolId = request.schoolId,
+                        departmentName = request.department,
+                        times = responses.timetables.last().timetableId
+                )
         return response
     }
 }
