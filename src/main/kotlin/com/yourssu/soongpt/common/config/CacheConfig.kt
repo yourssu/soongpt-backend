e0195dcbf19458cf4b07f2ce2f53783451ef6e0e
dwl21 <nggus5@gmail.com>
2026-02-06 15:22:38 +0900

    fix: Update course credits in target parser output for accuracy and consistency

--- src/main/kotlin/com/yourssu/soongpt/common/config/CacheConfig.kt
+++ src/main/kotlin/com/yourssu/soongpt/common/config/CacheConfig.kt
@@ -1,24 +1,8 @@
 package com.yourssu.soongpt.common.config
 
-import org.springframework.cache.CacheManager
-import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.concurrent.ConcurrentMapCache
-import org.springframework.cache.support.SimpleCacheManager
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableCaching
+// @EnableCaching // Caching is disabled
 class CacheConfig {
-    @Bean
-    fun cacheManager(): CacheManager {
-        val cacheManager = SimpleCacheManager()
-        cacheManager.setCaches(
-            listOf(
-                ConcurrentMapCache("courseCache"),
-                ConcurrentMapCache("timetableCache"),
-            )
-        )
-        return cacheManager
-    }
 }
