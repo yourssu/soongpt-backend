80d551aefc0d1880c3c60b53ae83131f7de1fa53
chlee <chlee@student.42seoul.kr>
2026-02-07 14:47:19 +0900

    chore: delete LabTimetable Logic to cover 25-2 Frontend Recruting

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/dto/LabTimetableResult.kt
+++
@@ -1,9 +0,0 @@
-package com.yourssu.soongpt.domain.timetable.implement.dto
-
-import com.yourssu.soongpt.domain.course.implement.Course
-import com.yourssu.soongpt.domain.timetable.implement.Timetable
-
-data class LabTimetableResult(
-    val timetable: Timetable,
-    val courses: List<Course>,
-)
