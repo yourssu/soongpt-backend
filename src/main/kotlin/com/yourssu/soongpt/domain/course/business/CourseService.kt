38a265e0e03f26ae81dbd740bbd846e65548db53
chlee <chlee@student.42seoul.kr>
2026-02-08 22:26:54 +0900

    fix: wrong changes

--- src/main/kotlin/com/yourssu/soongpt/domain/course/business/CourseService.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/course/business/CourseService.kt
@@ -3,10 +3,9 @@ package com.yourssu.soongpt.domain.course.business
 import com.yourssu.soongpt.domain.course.business.dto.*
 import com.yourssu.soongpt.domain.course.business.query.FilterCoursesQuery
 import com.yourssu.soongpt.domain.course.business.query.SearchCoursesQuery
-import com.yourssu.soongpt.domain.course.implement.Course
 
 interface CourseService {
-    fun findAll(query: FilterCoursesQuery): List<Course>
+    fun findAll(query: FilterCoursesQuery): List<CourseResponse>
     fun search(query: SearchCoursesQuery): SearchCoursesResponse
     fun findAllByCode(codes: List<Long>): List<CourseDetailResponse>
     fun getFields(schoolId: Int?): Any
