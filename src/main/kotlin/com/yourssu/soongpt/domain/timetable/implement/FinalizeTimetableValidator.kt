6c39cdbc437d1851258c6d3e8b411a0c0464fc3f
chlee <chlee@student.42seoul.kr>
2026-02-08 16:41:56 +0900

    feat: 시간표 추천 로직

---
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/implement/FinalizeTimetableValidator.kt
@@ -0,0 +1,35 @@
+package com.yourssu.soongpt.domain.timetable.implement
+
+import com.yourssu.soongpt.domain.course.implement.Course
+import com.yourssu.soongpt.domain.timetable.implement.exception.TimetableConflictException
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class FinalizeTimetableValidator(
+    private val timetableCourseReader: TimetableCourseReader,
+    private val courseCandidateFactory: CourseCandidateFactory
+) {
+    fun validate(timetableId: Long, coursesToAdd: List<Course>) {
+        if (coursesToAdd.isEmpty()) {
+            return
+        }
+
+        // 1. 기반 시간표의 시간 정보(BitSet)를 가져옴
+        val baseCourses = timetableCourseReader.findAllCourseByTimetableId(timetableId)
+        val timetableBitSet = BitSet()
+        baseCourses.forEach { course ->
+            val courseCandidate = courseCandidateFactory.create(course)
+            timetableBitSet.or(courseCandidate.timeSlot)
+        }
+
+        // 2. 추가할 과목들과 시간 충돌 검사
+        coursesToAdd.forEach { course ->
+            val courseCandidate = courseCandidateFactory.create(course)
+            if (timetableBitSet.intersects(courseCandidate.timeSlot)) {
+                throw TimetableConflictException("시간이 겹치는 과목(${course.name})이 있어 시간표를 완성할 수 없습니다.")
+            }
+            timetableBitSet.or(courseCandidate.timeSlot) // 검사 통과한 과목은 다음 검사를 위해 BitSet에 추가
+        }
+    }
+}
