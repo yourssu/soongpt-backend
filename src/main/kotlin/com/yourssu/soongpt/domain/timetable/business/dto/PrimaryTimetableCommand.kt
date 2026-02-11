24d854fd13dc64f757203b486e7a73da784cb6ac
chlee <chlee@student.42seoul.kr>
2026-02-11 21:35:10 +0900

    chore: 시간표 요청/기본값 정리

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/dto/PrimaryTimetableCommand.kt
+++ src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/dto/PrimaryTimetableCommand.kt
@@ -2,10 +2,10 @@ package com.yourssu.soongpt.domain.timetable.business.dto
 
 data class PrimaryTimetableCommand(
     val userId: String = "anonymous", // TODO: userContext 가져오기 위한 key (sToken + ?)
-    val maxCredit: Int,
     val retakeCourses: List<SelectedCourseCommand> = emptyList(),
     val majorRequiredCourses: List<SelectedCourseCommand> = emptyList(),
     val majorElectiveCourses: List<SelectedCourseCommand> = emptyList(),
+    val majorBasicCourses: List<SelectedCourseCommand> = emptyList(),
     val doubleMajorCourses: List<SelectedCourseCommand> = emptyList(),
     val minorCourses: List<SelectedCourseCommand> = emptyList(),
     val teachingCourses: List<SelectedCourseCommand> = emptyList(),
