58692d78e3e29c83deb0040661d3702a0ee4d18a
chlee <chlee@student.42seoul.kr>
2026-02-12 06:10:52 +0900

    chore: optimizing import

--- src/main/kotlin/com/yourssu/soongpt/common/infrastructure/notification/Notification.kt
+++ src/main/kotlin/com/yourssu/soongpt/common/infrastructure/notification/Notification.kt
@@ -3,7 +3,6 @@ package com.yourssu.soongpt.common.infrastructure.notification
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.yourssu.soongpt.common.infrastructure.dto.TimetableCreatedAlarmRequest
 import com.yourssu.soongpt.domain.contact.business.dto.ContactResponse
-import com.yourssu.soongpt.domain.timetable.business.dto.TimetableResponses
 import io.github.oshai.kotlinlogging.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
