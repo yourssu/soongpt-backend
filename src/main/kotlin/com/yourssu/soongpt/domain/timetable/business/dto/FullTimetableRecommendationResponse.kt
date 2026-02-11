7fae37c1a8c646d1d627e687f769bcacddf5b7b6
chlee <chlee@student.42seoul.kr>
2026-02-12 06:09:59 +0900

    chore: remove unused dto, userContext mock data service

--- src/main/kotlin/com/yourssu/soongpt/domain/timetable/business/dto/FullTimetableRecommendationResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.yourssu.soongpt.domain.timetable.business.dto
-
-/**
- * 1차 추천과 대안 추천을 모두 포함하는 통합 응답 DTO
- */
-data class FullTimetableRecommendationResponse(
-    val primaryTimetable: TimetableResponse,
-    val alternativeSuggestions: List<RecommendationDto>
-)
